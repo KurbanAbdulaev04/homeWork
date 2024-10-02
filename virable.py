b085723986f78a14e5ebb1a4b75a92cd5a34131a
NabruK <kurban2004a@gmail.com>
2024-10-02 10:39:22 +0300

    Add files via upload

---
+++ virable.py
@@ -0,0 +1,6 @@
+number_home_work = 12
+number_hous = 1.5
+name_cours = 'Python'
+time_of_task = number_hous/number_home_work
+
+print('Курс: ' + name_cours + ', всего задач: ' + str(number_home_work) + ', затрачено часов: ' + str(number_hous) + ', среднее время выполнения ' + str(time_of_task) + '.')

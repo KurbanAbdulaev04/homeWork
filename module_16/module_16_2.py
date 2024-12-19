fe01d6e36f8d38548aaee00c0bf98339a37a3012
Kurban <kurban2004a@gmail.com>
2024-12-19 12:01:35 +0300

    Завершен 17 модуль

--- module_16/module_16_2.py
+++ module_16/module_16_2.py
@@ -13,10 +13,10 @@ async def user_admin() -> str:
     return 'Вы вошли как администратор'
 
 @app.get('/user/{user_id}')
-async def id_user(user_id: int = Path(ge=1, le=100, description='Enter User ID', example="1")):
+async def id_user(user_id: Annotated[int, Path(ge=1, le=100, description='Enter User ID', example="1")]):
     return f'Вы вошли как пользователь № {user_id}'
 
 @app.get('/user/{username}/{age}')
 async def user_params(username: Annotated[str, Path(min_length=5, max_length=20, description='Enter username', example='UrbanUser')],
-                      age: int = Path(ge=18, le=120, description='Enter age', example=24)):
+                      age: Annotated[int, Path(ge=18, le=120, description='Enter age', example=24)]):
     return f'Информация о пользователе. Имя: {username}, Возраст: {age}'
---
+++ module_17/app/main.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+from app.routers import user
+from app.routers import task
+
+
+app = FastAPI()
+
+
+@app.get('/')
+async def welcome():
+    return {'message': 'Welcome to Taskmanager'}
+
+
+app.include_router(user.user_router)
+app.include_router(task.task_router)
+
